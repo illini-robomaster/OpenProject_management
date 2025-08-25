6677fe6b8496fefdc3b190e905db47d3c6ab2c65
Ziyi Wang <anderprima54@gmail.com>
2025-08-25 01:35:11 -0500

    update node services

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (_, res) => res.send('OK'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log('listening on', PORT));
